7ebcef1bd3fbbd35d049ce127e7c71e066d496bd
marymusa <maria.munoz.santos@gmail.com>
2024-09-24 16:57:45 +0200

    chore: add app.yaml and logging

--- src/main/java/com/marymusa/boardgamejournal/BoardGameJournalApplication.java
+++ src/main/java/com/marymusa/boardgamejournal/BoardGameJournalApplication.java
@@ -1,13 +1,19 @@
 package com.marymusa.boardgamejournal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardGameJournalApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(BoardGameJournalApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting the BoardGameJournalApplication...");
 		SpringApplication.run(BoardGameJournalApplication.class, args);
+		logger.info("BoardGameJournalApplication has started successfully.");
 	}
 
 }
