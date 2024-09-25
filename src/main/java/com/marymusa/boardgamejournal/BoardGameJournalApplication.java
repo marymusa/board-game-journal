e5558e6be69202caa5f400a5bc59efc71c99aa33
marymusa <maria.munoz.santos@gmail.com>
2024-09-25 16:51:07 +0200

    ci: prepare for google app engine

--- src/main/java/com/marymusa/boardgamejournal/BoardGameJournalApplication.java
+++ src/main/java/com/marymusa/boardgamejournal/BoardGameJournalApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BoardGameJournalApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardGameJournalApplication.class);
@@ -15,5 +18,14 @@ public class BoardGameJournalApplication {
 		SpringApplication.run(BoardGameJournalApplication.class, args);
 		logger.info("BoardGameJournalApplication has started successfully.");
 	}
-
 }
+
+	// Add the controller.
+	@RestController
+	class HelloWorldController {
+		@GetMapping("/")
+		public String hello() {
+			return "hello Board Game Journal world!☀\uFE0F";
+		}
+	}
+
